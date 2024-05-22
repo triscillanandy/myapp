c92aff9c67cea03ab03f236b13622dd8fc95e4de
Maria Nandawula <110343215+triscillanandy@users.noreply.github.com>
2024-05-22 11:18:27 +0300

    new

--- app/Views/register.php
+++ app/Views/register.php
@@ -1,37 +1,62 @@
-<h3>Register</h3>
-<hr>
+<!DOCTYPE html>
+<html lang="en">
 
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Register</title>
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css">
+</head>
 
+<body>
+    <div class="container">
+        <div class="row d-flex align-items-center justify-content-center" style="min-height: 600px;">
+            <div class="card">
+                <div class="card-content">
+                    <div class="card-body shadow">
+                        <h3>Register</h3>
+                        <hr>
+                        <?php if (isset($_SESSION['success'])) { ?>
+                            <div class="alert alert-success" role="alert">
+                                <?php echo $_SESSION['success'];
+                                unset($_SESSION['success']);
+                                ?>
+                            </div>
+                        <?php } ?>
+                        <form action="<?= base_url('register') ?>" method="post">
+                            <?= csrf_field(); ?>
+                            <?= validation_list_errors() ?>
+                            <div class="form-group">
+                                <label for="firstname">First Name</label>
+                                <input type="text" class="form-control" name="firstname" id="firstname" value="<?= set_value('firstname') ?>" required>
+                            </div>
+                            <div class="form-group">
+                                <label for="lastname">Last Name</label>
+                                <input type="text" class="form-control" name="lastname" id="lastname" value="<?= set_value('lastname') ?>" required>
+                            </div>
+                            <div class="form-group">
+                                <label for="email">Email address</label>
+                                <input type="email" class="form-control" name="email" id="email" value="<?= set_value('email') ?>" required>
+                            </div>
+                            <div class="form-group">
+                                <label for="password">Password</label>
+                                <input type="password" class="form-control" name="password" id="password" required>
+                            </div>
+                            <div class="form-group">
+                                <label for="password_confirm">Confirm Password</label>
+                                <input type="password" class="form-control" name="password_confirm" id="password_confirm" required>
+                            </div>
+                            <div class="form-group">
+                                <button type="submit" class="btn btn-secondary shadow-sm">Register</button>
+                                <a href="<?= base_url('login') ?>">Already have an account</a>
+                            </div>
+                        </form>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</body>
 
-<form action="<?= base_url('register')?>" method="post">
-   <?= validation_list_errors() ?>
-    <div>
-        <label for="firstname">First Name</label>
-        <input type="text" name="firstname" id="firstname" value="<?= set_value('firstname') ?>">
-      
-    </div>
-    <div>
-        <label for="lastname">Last Name</label>
-        <input type="text" name="lastname" id="lastname" value="<?= set_value('lastname') ?>">
-       
-    </div>
-    <div>
-        <label for="email">Email address</label>
-        <input type="text" name="email" id="email" value="<?= set_value('email') ?>">
-   
-    </div>
-    <div>
-        <label for="password">Password</label>
-        <input type="password" name="password" id="password">
-      
-    </div>
-    <div>
-        <label for="password_confirm">Confirm Password</label>
-        <input type="password" name="password_confirm" id="password_confirm">
-   
-    </div>
-    <div>
-        <button type="submit" value="submit">Register</button>
-        <a href="<?= base_url('login')?>">Already have an account</a>
-    </div>
-</form>
+</html>
